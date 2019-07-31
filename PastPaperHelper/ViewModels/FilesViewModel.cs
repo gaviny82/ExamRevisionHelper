0a06399b632a1e42c3d0407d0578e7c49fc1a13b
GavinYou082 <gavinyou2002@gmail.com>
2019-07-31 16:03:13 +0800

    Add more controls to Files page

--- PastPaperHelper/ViewModels/FilesViewModel.cs
+++ PastPaperHelper/ViewModels/FilesViewModel.cs
@@ -1,13 +1,73 @@
-﻿using System;
+﻿using PastPaperHelper.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PastPaperHelper.ViewModels
 {
-    public class FilesViewModel
+    public class FilesViewModel : NotificationObject
     {
+        private ObservableCollection<ExamYearViewModel> _examYears = new ObservableCollection<ExamYearViewModel>();
+        public IEnumerable<ExamYearViewModel> ExamYears
+        {
+            get { return _examYears; }
+        }
+        public FilesViewModel()
+        {
+            ExamSeries = "-";
+            _examYears.Add(new ExamYearViewModel { Year = "2018" });
+        }
 
+        private string _examSeries;
+        public string ExamSeries
+        {
+            get { return _examSeries; }
+            set { _examSeries = value; RaisePropertyChangedEvent("ExamSeries"); }
+        }
+
+        private SubjectSource _selectedSubject;
+        public SubjectSource SelectedSubject
+        {
+            get { return _selectedSubject; }
+            set { _selectedSubject = value; RaisePropertyChangedEvent("SelectedSubject"); }
+        }
+
+
+    }
+
+    public class ExamYearViewModel : NotificationObject
+    {
+        public string Year { get; set; }
+        public ExamSeriesViewModel Spring { get; set; }
+        public ExamSeriesViewModel Summer { get; set; }
+        public ExamSeriesViewModel Winter { get; set; }
+
+    }
+
+    public class ExamSeriesViewModel : NotificationObject
+    {
+        public string Year { get; set; }
+        public ExamSeries ExamSeries { get; set; }
+        public SubjectSource Subject { get; set; }
+        public override string ToString()
+        {
+            string str = "";
+            switch (ExamSeries)
+            {
+                case ExamSeries.Spring:
+                    str = "March";
+                    break;
+                case ExamSeries.Summer:
+                    str = "May/June";
+                    break;
+                case ExamSeries.Winter:
+                    str = "Oct/Nov";
+                    break;
+            }
+            return Year + " " + str;
+        }
     }
 }
--- PastPaperHelper/Views/FilesView.xaml.cs
+++ PastPaperHelper/Views/FilesView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using PastPaperHelper.ViewModels;
+﻿using PastPaperHelper.Models;
+using PastPaperHelper.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
