32dacb0b2c259e6ec56eb3532bc81fa648a353a4
GavinYou082 <gavinyou082@gmail.com>
2019-08-24 17:26:15 +0800

    working on the new paper source

--- PastPaperHelper/Sources/PaperSourcePapaCambridge.cs
+++ PastPaperHelper/Sources/PaperSourcePapaCambridge.cs
@@ -15,7 +15,49 @@ namespace PastPaperHelper.Sources
 
         public override PaperRepository GetPapers(Subject subject, string url)
         {
-            throw new NotImplementedException();
+            PaperRepository repo = new PaperRepository(subject);
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            HtmlNodeCollection examNodes = doc.DocumentNode.SelectNodes("/html/body/section[2]/div/div/div[1]/div[1]/div/div[2]/div/div/section/div/div/div/div/div/div/div/div/table/tbody/tr[@class=\"point\"]");
+
+            string resUrl1 = "", resUrl2 = "";
+            List<Exam> examList = new List<Exam>();
+            foreach (HtmlNode examNode in examNodes)
+            {
+                HtmlNode linkNode = examNode.ChildNodes.FindFirst("td").ChildNodes.FindFirst("a");
+                string examUrl = linkNode.Attributes["href"].Value;
+                HtmlNode textNode = linkNode.ChildNodes.FindFirst("label");
+                string label = textNode.InnerText;
+
+                if (label.Contains("&"))
+                {
+                    if (string.IsNullOrEmpty(resUrl1)) resUrl1 = examUrl;
+                    else resUrl2 = examUrl;
+                    continue;
+                }
+
+                string substr = label.Substring(5);
+
+                ExamSeries series;
+                if (substr.Contains("Mar")) series = ExamSeries.Spring;
+                else if (substr.Contains("Jun")) series = ExamSeries.Summer;
+                else series = ExamSeries.Winter;
+
+                Exam exam = new Exam
+                {
+                    Subject = subject,
+                    Year = label.Substring(0, 4),
+                    Series = series,
+                };
+
+                HtmlDocument examPage = web.Load(examUrl);
+                HtmlNodeCollection paperNodes = examPage.DocumentNode.SelectNodes("//*[@id=\"data\"]/div/div/div[1]/div/table/tbody//td[@data-name!=\"..\"]");
+
+                examList.Add(exam);
+            }
+            //read specimen papers and syllabus
+
+            return repo;
         }
 
         public override Dictionary<Subject, string> GetSubjectUrlMap(Curriculums curriculum)
