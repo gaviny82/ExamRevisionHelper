8b5bcd758ee9bae65fae05ff148b3c3dcdd75e55
GavinYou082 <gavinyou082@gmail.com>
2019-09-02 23:38:18 +0800

    Minor UI improvements

---
+++ PastPaperHelper/ViewModels/SubjectViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PastPaperHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastPaperHelper.ViewModels
+{
+    class SubjectViewModel : NotificationObject
+    {
+        public Subject Subject { get; set; }
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set { _isLoading = value; RaisePropertyChangedEvent("IsLoading"); }
+        }
+
+    }
+}
