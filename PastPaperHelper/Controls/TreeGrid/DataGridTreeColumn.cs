cbdd76f9ddecb3675ae5eabd8f6a07eefc4018c4
GavinYou082 <gavinyou2002@gmail.com>
2019-10-19 15:15:42 +0800

    Imported TreeGrid to LocalStorage page

---
+++ PastPaperHelper/Controls/TreeGrid/DataGridTreeColumn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Test.TreeGrid
+{
+    public class DataGridTreeColumn : DataGridTemplateColumn
+    {
+        public DataGridTreeColumn()
+        {
+            
+        }
+    }
+}
