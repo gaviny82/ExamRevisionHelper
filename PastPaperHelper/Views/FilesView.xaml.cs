0d53a4d59a20d32727004a308609c34c5a411003
GavinYou082 <gavinyou082@gmail.com>
2019-12-17 20:16:21 +0800

    Download Test

--- PastPaperHelper/Views/FilesView.xaml.cs
+++ PastPaperHelper/Views/FilesView.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using PastPaperHelper.Models;
 using PastPaperHelper.ViewModels;
 using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Linq;
+using System.Net;
+using PastPaperHelper.Sources;
 
 namespace PastPaperHelper.Views
 {
@@ -34,6 +39,60 @@ namespace PastPaperHelper.Views
         private void ViewPaper_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             Process.Start(((sender as Button).DataContext as Paper).Url);
+        }
+
+        private void Download_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PaperRepository repo = SubscriptionManager.Subscription[(Subject)subjectSelector.SelectedItem];
+            Task.Run(() =>
+            {
+                string path = Properties.Settings.Default.Path;
+                path += $"\\{repo.Subject.SyllabusCode} {(repo.Subject.Curriculum == Curriculums.ALevel ? "AL" : "GCSE")} {repo.Subject.Name}";
+                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+                foreach (ExamYear year in repo)
+                {
+                    if (year.Specimen != null) DownloadPaper(year.Specimen, path);
+                    if (year.Spring != null) DownloadPaper(year.Spring, path);
+                    if (year.Summer != null) DownloadPaper(year.Summer, path);
+                    if (year.Winter != null) DownloadPaper(year.Winter, path);
+                }
+            });
+        }
+        WebClient client = new WebClient();
+        private void DownloadPaper(Exam exam, string dir)
+        {
+            string ser = "";
+            switch (exam.Series)
+            {
+                case ExamSeries.Spring:
+                    ser = "March";
+                    break;
+                case ExamSeries.Summer:
+                    ser = "May-June";
+                    break;
+                case ExamSeries.Winter:
+                    ser = "Oct-Nov";
+                    break;
+                case ExamSeries.Specimen:
+                    ser = "Specimen";
+                    break;
+                default:
+                    break;
+            }
+            dir += $"\\{exam.Year} {ser}";
+            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+            var lst = (from comp in exam.Components select comp.Variants);
+            foreach (var varients in lst)
+            {
+                foreach (var item in varients)
+                {
+                    foreach (Paper paper in item.Papers)
+                    {
+                        client.DownloadFile(paper.Url, dir + $"\\{paper.Url.Split('/').Last()}");
+                    }
+                }
+            }
         }
     }
 }
