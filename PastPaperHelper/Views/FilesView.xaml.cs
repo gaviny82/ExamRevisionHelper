c61b7ed5eb9b4019acdd1475ebe47aced77d9015
GavinYou082 <gavinyou2002@gmail.com>
2019-12-28 18:14:04 +0800

    Download test (parallel)

--- PastPaperHelper/Views/FilesView.xaml.cs
+++ PastPaperHelper/Views/FilesView.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Input;
 using System.Linq;
 using System.Net;
 using PastPaperHelper.Sources;
+using System.Collections.Generic;
+using System.Windows;
+using System.Threading;
 
 namespace PastPaperHelper.Views
 {
@@ -44,22 +47,79 @@ namespace PastPaperHelper.Views
         private void Download_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             PaperRepository repo = SubscriptionManager.Subscription[(Subject)subjectSelector.SelectedItem];
+            progress.Value = 0;
+            error.Text = "";
             Task.Run(() =>
             {
-                string path = Properties.Settings.Default.Path;
-                path += $"\\{repo.Subject.SyllabusCode} {(repo.Subject.Curriculum == Curriculums.ALevel ? "AL" : "GCSE")} {repo.Subject.Name}";
-                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-
-                foreach (ExamYear year in repo)
+                if (failedTasks.Count != 0)
                 {
-                    if (year.Specimen != null) DownloadPaper(year.Specimen, path);
-                    if (year.Spring != null) DownloadPaper(year.Spring, path);
-                    if (year.Summer != null) DownloadPaper(year.Summer, path);
-                    if (year.Winter != null) DownloadPaper(year.Winter, path);
+                    downloadTasks.AddRange(failedTasks);
+                    failedTasks.Clear();
+                }
+                else
+                {
+                    string path = Properties.Settings.Default.Path;
+                    path += $"\\{repo.Subject.SyllabusCode} {(repo.Subject.Curriculum == Curriculums.ALevel ? "AL" : "GCSE")} {repo.Subject.Name}";
+                    if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+                    foreach (ExamYear year in repo)
+                    {
+                        if (year.Specimen != null) DownloadPaper(year.Specimen, path);
+                        if (year.Spring != null) DownloadPaper(year.Spring, path);
+                        if (year.Summer != null) DownloadPaper(year.Summer, path);
+                        if (year.Winter != null) DownloadPaper(year.Winter, path);
+                    }
+                }
+
+                int part = downloadTasks.Count / 5;
+                Task.Factory.StartNew(() =>
+                {
+                    progress.Maximum = downloadTasks.Count;
+                }, new CancellationTokenSource().Token, TaskCreationOptions.None, MainWindow.SyncContextTaskScheduler);
+                download(0, part, client1);
+                download(part, part * 2, client2);
+                download(part * 2, part * 3, client3);
+                download(part * 3, part * 4, client4);
+                download(part * 4, downloadTasks.Count, client5);
+            });
+        }
+        public void download(int start, int end, WebClient client)
+        {
+            Task.Run(() =>
+            {
+                for (int i = start; i < end; i++)
+                {
+                    try
+                    {
+                        client.DownloadFile(downloadTasks[i].Item1, downloadTasks[i].Item2);
+                    }
+                    catch (System.Exception)
+                    {
+                        failedTasks.Add(downloadTasks[i]);
+                        Task.Factory.StartNew(() =>
+                        {
+                            error.Text += "\n" + downloadTasks[i].Item1;
+                        }, new CancellationTokenSource().Token, TaskCreationOptions.None, MainWindow.SyncContextTaskScheduler);
+                        continue;
+                    }
+                    finally
+                    {
+                        Task.Factory.StartNew(() =>
+                        {
+                            progress.Value += 1;
+                        }, new CancellationTokenSource().Token, TaskCreationOptions.None, MainWindow.SyncContextTaskScheduler);
+                    }
                 }
             });
         }
-        WebClient client = new WebClient();
+
+        List<(string, string)> downloadTasks = new List<(string, string)>();
+        List<(string, string)> failedTasks = new List<(string, string)>();
+        WebClient client1 = new WebClient();
+        WebClient client2 = new WebClient();
+        WebClient client3 = new WebClient();
+        WebClient client4 = new WebClient();
+        WebClient client5 = new WebClient();
         private void DownloadPaper(Exam exam, string dir)
         {
             string ser = "";
@@ -89,7 +149,7 @@ namespace PastPaperHelper.Views
                 {
                     foreach (Paper paper in item.Papers)
                     {
-                        client.DownloadFile(paper.Url, dir + $"\\{paper.Url.Split('/').Last()}");
+                        downloadTasks.Add((paper.Url, dir + $"\\{paper.Url.Split('/').Last()}"));
                     }
                 }
             }
