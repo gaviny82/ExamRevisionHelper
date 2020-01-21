11a0a44d534179c7e45ee3147d67665a9eb303f4
GavinYou082 <gavinyou2002@gmail.com>
2020-01-21 22:09:33 +0800

    Added DownloadTask viewmodel

---
+++ PastPaperHelper/ViewModels/DownloadTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastPaperHelper.ViewModels
+{
+    public enum DownloadTaskState { Pending, Completed, Error }
+    public class DownloadTask : NotificationObject
+    {
+        public string FileName { get; set; }
+
+        private byte _progress;
+        public byte Progress
+        {
+            get { return _progress; }
+            set { _progress = value; RaisePropertyChangedEvent(nameof(Progress)); }
+        }
+
+        private DownloadTaskState _state;
+        public DownloadTaskState State
+        {
+            get { return _state; }
+            set { _state = value; RaisePropertyChangedEvent(nameof(State)); }
+        }
+
+        public string ResourceUrl { get; set; }
+
+        public string LocalPath { get; set; }
+    }
+}
