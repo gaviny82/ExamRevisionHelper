1f9ac3206dade81eb1e036e1aeb8e65e46c0f8f5
GavinYou082 <gavinyou2002@gmail.com>
2020-07-10 22:04:21 +0800

    Add countdown

--- PastPaperHelper/ViewModels/PracticeViewModel.cs
+++ PastPaperHelper/ViewModels/PracticeViewModel.cs
@@ -1,11 +1,16 @@
-﻿using PastPaperHelper.Models;
+﻿using PastPaperHelper.Core.Tools;
+using PastPaperHelper.Models;
 using Prism.Mvvm;
 using Prism.Regions;
+using Spire.Pdf;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 
 namespace PastPaperHelper.ViewModels
@@ -28,6 +33,13 @@ namespace PastPaperHelper.ViewModels
             set { SetProperty(ref _message, value); }
         }
 
+        private TimeSpan _countdown;
+        public TimeSpan Countdown
+        {
+            get { return _countdown; }
+            set { SetProperty(ref _countdown, value); }
+        }
+
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
             return true;
@@ -45,7 +57,31 @@ namespace PastPaperHelper.ViewModels
                 IsMockStarted = true;
                 Message = "When your are ready for the exam, click the button below to start the timer.";
                 MockPaper = navigationContext.Parameters["MockPaper"] as Variant;
-                //TODO: start mock exam
+
+                foreach (Paper item in MockPaper.Papers)
+                {
+                    var filename = item.Url?.Split('/').Last();
+                    if (item.Type == ResourceType.QuestionPaper)
+                    {
+                        Process.Start(PastPaperHelperCore.LocalFiles[filename]);
+                        using PdfDocument doc = new PdfDocument(PastPaperHelperCore.LocalFiles[filename]);
+                        string txt = doc.Pages[0]?.ExtractText();
+                        MatchCollection matches = Regex.Matches(txt, @"([0-9]+\s)(?:hour(s)?|minutes)((\s[0-9]+\s)(?:hour(s)?|minutes))*");
+                        string[] timeData = matches[0]?.Value.Replace(" hours", "").Replace(" hour", "").Replace(" minutes","").Split(' ');
+                        Countdown = new TimeSpan();
+
+                        Timer timer = new Timer();
+                        
+                    }
+                    else if (item.Type == ResourceType.Insert)
+                    {
+                        Process.Start(PastPaperHelperCore.LocalFiles[filename]);
+                    }
+                    else if (item.Type == ResourceType.ListeningAudio)
+                    {
+                        Process.Start(PastPaperHelperCore.LocalFiles[filename]);
+                    }
+                }
             }
         }
     }
